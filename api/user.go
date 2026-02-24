8a092b9d2860ef4a65f6e9b89ac0ae6272f42ce4
swayam kaushal <swayam.kaushal@capgemini.com>
2026-02-24 18:29:06 +0000

    fixed basic validation error

--- api/user.go
+++ api/user.go
@@ -50,7 +50,7 @@ func (server *Server) createUser(ctx *gin.Context) {
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
 			switch pqErr.Code.Name() {
-			case "unique_voilation":
+			case "unique_violation":
 				ctx.JSON(http.StatusForbidden, errorResponse(err))
 				return
 			}
